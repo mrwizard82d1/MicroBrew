7ffd0150caa65feba08158fe5f34cc3699e7c9b8
Larry Jones <jones@kappaeng.com>
2023-05-22 13:30:33 -0500

    Add TypeScript consumer and new function

--- typescript/no-bs-ts/episode-30/mylib/dist/index.js
+++ typescript/no-bs-ts/episode-30/mylib/dist/index.js
@@ -1,5 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.introduceMyself = void 0;
+exports.borgName = exports.introduceMyself = void 0;
 const introduceMyself = (first, last) => `Hello, ${first} ${last}`;
 exports.introduceMyself = introduceMyself;
+const borgName = () => {
+    const members = Math.round(5 + (Math.random() * 5)) + 1;
+    const member = Math.floor(Math.random() * members + 1);
+    return `Your Borg name is ${member} of ${members}`;
+};
+exports.borgName = borgName;
