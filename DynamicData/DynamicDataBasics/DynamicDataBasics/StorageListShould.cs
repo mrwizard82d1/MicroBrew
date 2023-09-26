04cb52cddb2433dde195c6b5a4c6860f48142901
Larry Jones <jones@kappaeng.com>
2023-09-25 22:00:20 -0500

    Implement basic tests for DynamicData
    Implement tests for:
      - `Add()`
      - `AddRange()`
      - `Remove()`

--- DynamicData/DynamicDataBasics/DynamicDataBasics/StorageListShould.cs
+++ DynamicData/DynamicDataBasics/DynamicDataBasics/StorageListShould.cs
@@ -1,14 +1,54 @@
+using DynamicData.Kernel;
+
 namespace DynamicDataBasics;
 
+using DynamicData;
 using NUnit.Framework;
 
 [TestFixture]
-public class Tests
+public class StorageListShould
 {
     [TestCase]
     public void PassACanaryTest()
     {
         const int toTest = 2 + 2;
         Assert.That(toTest, Is.EqualTo(4));
+    }
+
+    [TestCase]
+    public void AddIntAddsAnIntegerToTheList()
+    {
+        var sut = new SourceList<Int32>();
+        sut.Add(79);
+
+        var expected = new[]
+        {
+            79,
+        };
+        Assert.That(sut.Items, Is.EqualTo(expected));
+    }
+
+    [TestCase]
+    public void AddManyIntsAddsManyIntegerToTheList()
+    {
+        var sut = new SourceList<Int32>();
+        var expected = new[] { 58, 34, 6, };
+        sut.AddRange(expected);
+
+        Assert.That(sut.Items, Is.EqualTo(expected));
+    }
+
+    [TestCase]
+    public void RemoveIntRemovesIntegerFromTheList()
+    {
+        var sut = new SourceList<Int32>();
+        var original = new[] { 55, 96, 86, };
+        sut.AddRange(original);
+
+        sut.Remove(86);
+
+        var expected = original.AsList();
+        expected.Remove(86);
+        Assert.That(sut.Items, Is.EqualTo(expected));
     }
 }
