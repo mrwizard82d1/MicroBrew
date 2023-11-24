0ded7ba858654ce5afd0630ce0ad652e09f19017
Larry Jones <jones@kappaeng.com>
2023-11-24 16:38:16 -0600

    WPF supports event handling

---
+++ wpf/WpfTutorial/Xaml.EventsSample/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Xaml.EventsSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainGrid_OnMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            var position = e.GetPosition(this);
+            MessageBox.Show($"You clicked me at ({position.X}, {position.Y}) in the grid (and main window)");
+        }
+    }
+}
