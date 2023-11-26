13c04afaa258ff0ab7e21ed40d96346d1f723f75
Larry Jones <jones@kappaeng.com>
2023-11-25 19:24:57 -0600

    Example: using resources in code-behind

---
+++ wpf/WpfTutorial/ResourcesInCodeBehind/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace ResourcesInCodeBehind
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonClickMe_Click(object sender, RoutedEventArgs e)
+        {
+            switch (ListBoxResult.Items.IsEmpty)
+            {
+                case true:
+                    ListBoxResult.Items.Add(PanelMain.FindResource("StringPanel").ToString());
+                    ListBoxResult.Items.Add(FindResource("StringWindow").ToString());
+                    ListBoxResult.Items.Add(Application.Current.FindResource("StringApp")?.ToString());
+                    break;
+            }
+        }
+    }
+}
