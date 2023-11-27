5461e4a7f6c3f515c7b725615674ed2256974e04
Larry Jones <jones@kappaeng.com>
2023-11-27 14:29:12 -0600

    TextBlock - HyperLink example (BROKE)

---
+++ wpf/WpfTutorial/TextBlockHyperlinkSample/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Navigation;
+
+namespace TextBlockHyperlinkSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Hyperlink_OnRequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            // Navigate to the specified (absolute) Uri.
+            System.Diagnostics.Process.Start(e.Uri.AbsoluteUri);
+        }
+    }
+}
