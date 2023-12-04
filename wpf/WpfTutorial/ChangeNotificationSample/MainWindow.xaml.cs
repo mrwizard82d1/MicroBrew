8054a6168855691f4d6a22a81dc507e44237a638
Larry Jones <jones@kappaeng.com>
2023-12-04 13:52:11 -0600

    Write initial `ChangeNotificationSample`
    This implementation initializes everything correctly (initial data and
    bindings); however, when one updates the user collection by pressing one
    of the three buttons, nothing happens visually. (I have verified that
    the correct behavior seems to occur using the debugger.)
    
    This "oversight" will be fixed in the next two updates.

---
+++ wpf/WpfTutorial/ChangeNotificationSample/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace ChangeNotificationSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        private List<User> _users = new();
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _users.Add(new User { Name = "John Doe" });
+            _users.Add(new User { Name = "Jane Doe" });
+
+            LblUsers.ItemsSource = _users;
+        }
+
+        private void BtnAddUser_OnClick(object sender, RoutedEventArgs e)
+        {
+            _users.Add(new User { Name = "New User" });
+        }
+
+        private void BtnChangeUser_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (LblUsers.SelectedItem != null)
+            {
+                ((LblUsers.SelectedItem as User)!).Name = "Random Name";
+            }
+        }
+
+        private void BtnDeleteUser_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (LblUsers.SelectedItem != null)
+            {
+                _users.Remove((LblUsers.SelectedItem as User)!);
+            }
+        }
+    }
+
+    public class User
+    {
+        public string Name { get; set; }
+    }
+}
