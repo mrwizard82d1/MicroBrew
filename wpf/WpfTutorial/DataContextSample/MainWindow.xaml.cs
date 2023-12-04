b9e46d58337fe2c3b92ebd7dab791b2ba9acd8c6
Larry Jones <jones@kappaeng.com>
2023-12-04 12:05:20 -0600

    Write DataContextSample

---
+++ wpf/WpfTutorial/DataContextSample/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+
+namespace DataContextSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            // Sets the `DataContext` property of this window to be this window itself.
+            // NOTE: By default `DataContext` is `null` if it is not set.
+            Debug.Assert(DataContext == null);
+            DataContext = this;
+        }
+    }
+}
